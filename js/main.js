7ce0e5fe70d5ac4dc38440d9815475a66b527b31
Derrick Fleming <derrick.fleming7@gmail.com>
2022-09-26 23:52:35 +0000

    Created XMLHttpRequest() for API and set response type to 'json'. #1

--- js/main.js
+++ js/main.js
@@ -3,3 +3,7 @@ var $villagerView = document.querySelector('#villager-view');
 $villagerView.addEventListener('click', function (event) {
   return event.target;
 });
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://acnhapi.com/v1/');
+xhr.responseType = 'json';
