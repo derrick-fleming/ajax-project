bd89e51892f8e6b0722dff292d0c2e018747072b
Derrick Fleming <derrick.fleming7@gmail.com>
2022-09-27 05:12:39 +0000

    Added a function for only 'alligators' since data places them in 'c' instead of 'a' group. #1

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var $villagerView = document.querySelector('#villager-view');
 var speciesList = [];
 var villagerList = null;
 
-var speciesNumber = 0;
+var speciesNumber = 50;
 
 function generateLink() {
   var $topLink = document.createElement('a');
@@ -22,6 +22,7 @@ xhr.responseType = 'json';
 
 function generateList(event) {
   villagerList = xhr.response;
+  generateAlligator();
   generateDomVillagersList();
   return villagerList;
 }
@@ -30,6 +31,9 @@ function generateDomVillagersList() {
 
   for (var i = 0; i < speciesNumber + 50; i++) {
     var villagerSpecies = villagerList[i].species;
+    if (villagerSpecies === 'Alligator') {
+      continue;
+    }
     var villagerIcon = villagerList[i].icon_uri;
     var villagerName = villagerList[i].name['name-USen'];
     if (!speciesList.includes(villagerSpecies)) {
@@ -89,3 +93,62 @@ function generateDomVillagersList() {
 
 xhr.addEventListener('load', generateList);
 xhr.send();
+
+function generateAlligator() {
+  for (var i = 93; i < 100; i++) {
+    var villagerSpecies = villagerList[i].species;
+    var villagerIcon = villagerList[i].icon_uri;
+    var villagerName = villagerList[i].name['name-USen'];
+    if (!speciesList.includes(villagerSpecies)) {
+      speciesList.push(villagerSpecies);
+
+      var $villagerSection = document.createElement('div');
+      $villagerSection.setAttribute('id', villagerSpecies);
+      $villagerSection.className = 'villager-section';
+
+      var $villagerContainerHeader = document.createElement('div');
+      $villagerContainerHeader.className = 'container species-header row';
+
+      var $headerContainer = document.createElement('div');
+      $headerContainer.className = 'header-container';
+
+      var $h1 = document.createElement('h1');
+      $h1.textContent = villagerSpecies;
+      $h1.className = 'species-header';
+
+      $headerContainer.appendChild($h1);
+      $villagerContainerHeader.appendChild($headerContainer);
+      $villagerContainerHeader.appendChild(generateLink());
+      $villagerSection.appendChild($villagerContainerHeader);
+      $villagerView.appendChild($villagerSection);
+
+      var $villagerContainerSpeciesList = document.createElement('div');
+      $villagerContainerSpeciesList.className = 'container species-list row';
+    }
+
+    var $villagerColumn = document.createElement('div');
+    $villagerColumn.className = 'column-one-third center';
+    $villagerColumn.setAttribute('id', villagerName);
+
+    var $anchorVillager = document.createElement('a');
+
+    var $villagerIcon = document.createElement('img');
+    $villagerIcon.setAttribute('src', villagerIcon);
+    $villagerIcon.className = 'villager-icon';
+    $villagerIcon.setAttribute('alt', villagerName);
+
+    var $villagerName = document.createElement('h2');
+    $villagerName.className = 'villager-name';
+    $villagerName.textContent = villagerName;
+
+    $anchorVillager.appendChild($villagerIcon);
+    $anchorVillager.appendChild($villagerName);
+    $villagerColumn.appendChild($anchorVillager);
+    $villagerContainerSpeciesList.appendChild($villagerColumn);
+
+    if (villagerSpecies !== villagerList[i + 1].species || i === speciesNumber + 49) {
+      $villagerSection.appendChild($villagerContainerSpeciesList);
+    }
+  }
+
+}
