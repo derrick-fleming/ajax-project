60a86b22cd0687474ab69464065214556424b7b0
Derrick Fleming <derrick.fleming7@gmail.com>
2022-09-27 22:22:25 +0000

    Changed alphabetical ordering of api data  to avoid having separate functions for eagles and alligators. #1

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 var $villagerView = document.querySelector('#villager-view');
 var $loadMoreLink = document.querySelector('.load-link');
 var $scrollPopUp = document.querySelector('#scroll-info');
+var speciesList = [];
+var villagerList = null;
+var speciesNumber = 0;
 
 $scrollPopUp.addEventListener('click', function () {
   if (event.target.tagName === 'I') {
     $scrollPopUp.className = 'hidden';
   }
 });
-
-var speciesList = [];
-var villagerList = null;
-var speciesNumber = 0;
 
 var xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://acnhapi.com/v1a/villagers');
@@ -20,7 +19,7 @@ xhr.addEventListener('load', generateList);
 xhr.send();
 
 function generateList(event) {
-  villagerList = xhr.response;
+  villagerList = xhr.response.sort(function (a, b) { return a.species.localeCompare(b.species); });
   generateDomVillagersList();
   return villagerList;
 }
@@ -35,19 +34,8 @@ $loadMoreLink.addEventListener('click', function () {
 function generateDomVillagersList() {
 
   for (var i = speciesNumber; i < speciesNumber + 100; i++) {
-    if (i === 0) {
-      generateAlligator();
-    }
-
-    if (i === 143) {
-      generateEagle();
-    }
 
     var villagerSpecies = villagerList[i].species;
-
-    if (villagerSpecies === 'Alligator' || villagerSpecies === 'Eagle') {
-      continue;
-    }
 
     var villagerIcon = villagerList[i].icon_uri;
     var villagerName = villagerList[i].name['name-USen'];
@@ -119,121 +107,6 @@ function generateDomVillagersList() {
   }
   speciesNumber += 100;
   return speciesNumber;
-}
-
-function generateAlligator() {
-  for (var i = 93; i < 100; i++) {
-    var villagerSpecies = villagerList[i].species;
-    var villagerIcon = villagerList[i].icon_uri;
-    var villagerName = villagerList[i].name['name-USen'];
-    if (!speciesList.includes(villagerSpecies)) {
-      speciesList.push(villagerSpecies);
-
-      var $villagerSection = document.createElement('div');
-      $villagerSection.setAttribute('id', villagerSpecies);
-      $villagerSection.className = 'villager-section';
-
-      var $villagerContainerHeader = document.createElement('div');
-      $villagerContainerHeader.className = 'container species-header row';
-
-      var $headerContainer = document.createElement('div');
-      $headerContainer.className = 'header-container';
-
-      var $h1 = document.createElement('h1');
-      $h1.textContent = villagerSpecies;
-      $h1.className = 'species-header';
-
-      $headerContainer.appendChild($h1);
-      $villagerContainerHeader.appendChild($headerContainer);
-      $villagerSection.appendChild($villagerContainerHeader);
-      $villagerView.appendChild($villagerSection);
-
-      var $villagerContainerSpeciesList = document.createElement('div');
-      $villagerContainerSpeciesList.className = 'container species-list row';
-    }
-
-    var $villagerColumn = document.createElement('div');
-    $villagerColumn.className = 'column-one-third center';
-    $villagerColumn.setAttribute('id', villagerName);
-
-    var $anchorVillager = document.createElement('a');
-
-    var $villagerIcon = document.createElement('img');
-    $villagerIcon.setAttribute('src', villagerIcon);
-    $villagerIcon.className = 'villager-icon';
-    $villagerIcon.setAttribute('alt', villagerName);
-
-    var $villagerName = document.createElement('h2');
-    $villagerName.className = 'villager-name';
-    $villagerName.textContent = villagerName;
-
-    $anchorVillager.appendChild($villagerIcon);
-    $anchorVillager.appendChild($villagerName);
-    $villagerColumn.appendChild($anchorVillager);
-    $villagerContainerSpeciesList.appendChild($villagerColumn);
-
-    if (i === 99) {
-      $villagerSection.appendChild($villagerContainerSpeciesList);
-    }
-  }
-
-}
-
-function generateEagle() {
-  for (var i = 279; i < 288; i++) {
-    var villagerSpecies = villagerList[i].species;
-    var villagerIcon = villagerList[i].icon_uri;
-    var villagerName = villagerList[i].name['name-USen'];
-    if (!speciesList.includes(villagerSpecies)) {
-      speciesList.push(villagerSpecies);
-
-      var $villagerSection = document.createElement('div');
-      $villagerSection.setAttribute('id', villagerSpecies);
-      $villagerSection.className = 'villager-section';
-
-      var $villagerContainerHeader = document.createElement('div');
-      $villagerContainerHeader.className = 'container species-header row';
-
-      var $headerContainer = document.createElement('div');
-      $headerContainer.className = 'header-container';
-
-      var $h1 = document.createElement('h1');
-      $h1.textContent = villagerSpecies;
-      $h1.className = 'species-header';
-
-      $headerContainer.appendChild($h1);
-      $villagerContainerHeader.appendChild($headerContainer);
-      $villagerSection.appendChild($villagerContainerHeader);
-      $villagerView.appendChild($villagerSection);
-
-      var $villagerContainerSpeciesList = document.createElement('div');
-      $villagerContainerSpeciesList.className = 'container species-list row';
-    }
-
-    var $villagerColumn = document.createElement('div');
-    $villagerColumn.className = 'column-one-third center';
-    $villagerColumn.setAttribute('id', villagerName);
-
-    var $anchorVillager = document.createElement('a');
-
-    var $villagerIcon = document.createElement('img');
-    $villagerIcon.setAttribute('src', villagerIcon);
-    $villagerIcon.className = 'villager-icon';
-    $villagerIcon.setAttribute('alt', villagerName);
-
-    var $villagerName = document.createElement('h2');
-    $villagerName.className = 'villager-name';
-    $villagerName.textContent = villagerName;
-
-    $anchorVillager.appendChild($villagerIcon);
-    $anchorVillager.appendChild($villagerName);
-    $villagerColumn.appendChild($anchorVillager);
-    $villagerContainerSpeciesList.appendChild($villagerColumn);
-
-    if (i === 287) {
-      $villagerSection.appendChild($villagerContainerSpeciesList);
-    }
-  }
 }
 
 function generateLink() {
