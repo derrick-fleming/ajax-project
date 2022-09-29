be3e64f3a961afdc7d45c4684792b40237786dab
Derrick Fleming <derrick.fleming7@gmail.com>
2022-09-29 04:12:41 +0000

    stored favorites to local storage and checked that objects were successfully being stored. #3

--- js/data.js
+++ js/data.js
@@ -6,3 +6,15 @@ var data = {
   editing: null,
   nextFavorite: 1
 };
+
+var previousData = localStorage.getItem('acnh-villager-favorites');
+if (previousData !== null) {
+  data = JSON.parse(previousData);
+}
+
+window.addEventListener('beforeunload', storeToLocalStorage);
+
+function storeToLocalStorage(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('acnh-villager-favorites', dataJSON);
+}
